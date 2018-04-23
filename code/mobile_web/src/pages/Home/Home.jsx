50be9e88741e1dafa188d3c61c073b847bbeaa0a
justinzzc <justin_zzc@163.com>
2018-04-24 06:43:56 +0800

    h5工程添加

---
+++ code/mobile_web/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class Home extends Component {
+  static displayName = 'Home';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="home-page" >
+    
+    </div>;
+  }
+}
